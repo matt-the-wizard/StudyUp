da7dd857b916fb7d49372925a3324286bfca6236
Dericus Paul <dpaul1@ggc.edu>
2015-10-12 20:57:07 +0000

    Working on Modeling students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,7 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def update
@@ -12,9 +13,27 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new _student_params
+    if @student.save
+      redirect_to @student
+    else
+      render 'new'
+    end
   end
 
   def new
     @student = Student.new
   end
+  
+  private 
+  
+  def _student_params
+    params.require(:student).permit(:first_name,
+                                    :last_name,
+                                    :username,
+                                    :password_digest,
+                                    :institution,
+                                    :school_of_study,
+                                    :discipline)
+  end 
 end
