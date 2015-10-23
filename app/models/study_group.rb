8161ffbef73281c2d24097e906c622d90f30a78a
dpaul1 <dpaul1@ggc.edu>
2015-10-22 22:51:27 -0400

    fix search for study groups

--- app/models/study_group.rb
+++ app/models/study_group.rb
@@ -13,11 +13,11 @@ class StudyGroup < ActiveRecord::Base
 
   def self.search(params)
     if params[:filter] == TITLE_FILTER
-      return Student.where("title LIKE '%#{params[:search]}%'").to_a
+      return StudyGroup.where("title LIKE '%#{params[:search]}%'").to_a
     elsif params[:filter] == TOPIC_FILTER
-      return Student.where("topic LIKE '%#{params[:search]}%'").to_a
+      return StudyGroup.where("topic LIKE '%#{params[:search]}%'").to_a
     else
-      return Student.where("institution LIKE '%#{params[:search]}%'").to_a
+      return StudyGroup.where("institution LIKE '%#{params[:search]}%'").to_a
     end
   end
 
