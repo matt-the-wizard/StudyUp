7157702f936c462cd74c30f451ab8756187a24c0
Matthew Berger <matthewberger@finsync.com>
2015-10-25 11:51:20 -0400

    setup having students join a group, added display attributes on the group view and model, fixed admin access, fixed editing group, and a lot more stuff

--- app/models/study_group.rb
+++ app/models/study_group.rb
@@ -8,9 +8,20 @@ class StudyGroup < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :topic
 
+  before_save :_validate_student_limit
+  before_save :_assign_admin_student
+
   TITLE_FILTER = "Title"
   TOPIC_FILTER  = "Topic"
   INSTITUTION_FILTER  = "Institution"
+
+  def student_in_group?(student)
+    self.students.where(id: student.id).first.present?
+  end
+
+  def available_space
+    self.student_limit.to_i - self.students.count.to_i
+  end
 
   def self.search(params)
     if params[:filter] == TITLE_FILTER
@@ -20,6 +31,21 @@ class StudyGroup < ActiveRecord::Base
     else
       return StudyGroup.where("institution LIKE '%#{params[:search]}%'").to_a
     end
+  end
+
+  private
+
+  def _validate_student_limit
+    if available_space == 0
+      errors.add(:students, "The student limit for this group is full, you cannot join this group.")
+      return false
+    else
+      return true
+    end
+  end
+
+  def _assign_admin_student
+    self.admin_student = self.students.first if self.admin_student.nil? && self.students.count.to_i > 0
   end
 
 end
