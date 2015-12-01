0ba24e34e60ef11f0d02614fdad2f1b0e77ca0fa
Matthew Berger <matthewberger@finsync.com>
2015-12-01 00:33:52 -0500

    refactored permitted params

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -23,12 +23,26 @@ class Api::StudentsController < ::ApplicationController
   end
 
   def create
-    @student = Student.new _student_params
+    @student = Student.new _permitted_attributes
     if @student.save
       render json: @student, serializer: StudentSerializer
     else
       render json: {"Study Up Authentication Error" => @student.errors}
     end
+  end
+
+  private
+
+  def _permitted_attributes
+    params.permit(:first_name,
+                  :last_name,
+                  :username,
+                  :password,
+                  :password_confirmation,
+                  :institution,
+                  :school_of_study,
+                  :discipline,
+                  :phone_number)
   end
 
 end
